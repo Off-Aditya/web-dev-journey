2493e1f5f2d7a196385c9c1637e282831f59ff92
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-08-18 22:37:16 +0530

    Updating...

---
+++ 02-javascript/if-else.js
@@ -0,0 +1,10 @@
+console.log('Checking if you can drive')
+let age = 69;
+
+if (age > 65) {
+    console.log('Please ask someone to Drive!')
+} else if (age > 18) {
+    console.log('You can Drive!')
+} else {
+    console.log('Use a Bicycle!')
+}

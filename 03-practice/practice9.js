c62a52697d7c03cf52d94b763ff046f94ff9046d
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-09-02 16:09:13 +0530

    practice

---
+++ 03-practice/practice9.js
@@ -0,0 +1,14 @@
+let age =1;
+let gender ='female';
+
+if (gender === 'female' ){
+    console.log('50% off')
+}
+
+if (age < 5){
+    console.log('100% off')
+} else if(age < 8){
+    console.log('50% off')
+} else if(age>65){
+    console.log('30%')
+}

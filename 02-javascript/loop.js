01a3db4799d7a621846e4fa564f80fe9fa002a21
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-09-18 22:57:28 +0530

    loop.js

---
+++ 02-javascript/loop.js
@@ -0,0 +1,19 @@
+// let num = 1;
+
+// while (num <= 1000) {
+//     console.log(num);
+//     num++;
+// }
+
+// let i = 0;
+// while (i < 1) {
+//     console.log(i);
+//     i = Math.random() * 2;
+// }
+// console.log('Final Value of i : ' + i);
+
+let n = 0;
+do {
+    console.log(n);
+    n++;
+} while (n < 15);

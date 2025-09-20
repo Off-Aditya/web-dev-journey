667d6c6eb4c7227513cf8c6a7afafcb28672b0d1
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-09-20 16:16:29 +0530

    loop.js

--- 02-javascript/loop.js
+++ 02-javascript/loop.js
@@ -12,8 +12,29 @@
 // }
 // console.log('Final Value of i : ' + i);
 
-let n = 0;
-do {
-    console.log(n);
-    n++;
-} while (n < 15);
+// let n = 0;
+// do {
+//     console.log(n);
+//     n++;
+// } while (n < 15);
+
+
+let nums = [4, 2, 3, 3, 11, 43, 21, 32, 11];
+let sum = 0; 
+for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i]);
+    sum += nums[i];
+}
+console.log(sum);
+
+let squares = [];
+for (let i = 0; i < nums.length; i++){
+    // squares[i] = nums[i] * nums[i];
+    squares.push(nums[i] * nums[i]);
+}
+console.log(squares);
+
+for (let i = 0; i < nums.length; i++) {
+    console.log(squares[i]);
+}
+
